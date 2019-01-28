f4d74c5a6201a7403ba6088042d3ad235d05e844
Corey H <c.l.halicke@gmail.com>
2019-01-28 00:31:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 
 def fizzbuzz(int)
-  if int / 3 = fixnums
+  if int  3 = 
     puts "Fizz"
-  elsif int % 5 == 0 
+  'elsif int % 5 == 0 
     puts "Buzz"
   elsif int % 5 == 0 && int % 3 == 0 
-    puts "FizzBuzz"
+    puts "FizzBuzz"'
   end
 end
