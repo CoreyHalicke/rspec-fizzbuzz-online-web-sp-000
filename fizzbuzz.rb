ba6309ccecc02195225bf2fe3ef121f8aadceafc
Corey H <c.l.halicke@gmail.com>
2019-01-28 00:24:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 
 def fizzbuzz(number)
-  if number % 3 == 0 
+  'if number % 3 == 0 
     puts "Fizz"
   elsif number % 5 == 0 
     puts "Buzz"
   elsif number % 5 == 0 && number % 3 == 0 
     puts "FizzBuzz"
-  end
+  end'
 end
